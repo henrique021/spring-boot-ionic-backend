d3388c632aafab6d98d8bfd50e7203b52eeeddd1
Henrique Ribeiro <hrgonnzaga@gmail.com>
2023-08-07 00:14:06 -0300

    repositorio e serviço

---
+++ src/main/java/com/hrgonzaga/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.hrgonzaga.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hrgonzaga.cursomc.domain.Categoria;
+import com.hrgonzaga.cursomc.repository.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar (Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		
+	}
+
+}
