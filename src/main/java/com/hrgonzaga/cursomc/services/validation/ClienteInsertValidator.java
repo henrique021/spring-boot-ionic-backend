088b45eeff3a61d64147eb8f3d60170a8c22ee5f
Henrique Ribeiro <hrgonnzaga@gmail.com>
2023-09-01 22:33:17 -0300

    validação email na inserçao

--- src/main/java/com/hrgonzaga/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/hrgonzaga/cursomc/services/validation/ClienteInsertValidator.java
@@ -3,8 +3,12 @@ package com.hrgonzaga.cursomc.services.validation;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.hrgonzaga.cursomc.domain.Cliente;
 import com.hrgonzaga.cursomc.domain.enums.TipoCliente;
 import com.hrgonzaga.cursomc.dto.ClienteNewDTO;
+import com.hrgonzaga.cursomc.repository.ClienteRepository;
 import com.hrgonzaga.cursomc.resources.exception.FieldMessage;
 import com.hrgonzaga.cursomc.services.validation.utils.BR;
 
@@ -12,6 +16,9 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +35,13 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getId()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj","Documento Invalido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			
+			list.add(new FieldMessage("email","email ja cadastrado"));
+			
 		}
 		
 // inclua os testes aqui, inserindo erros na lista
